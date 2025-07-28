b25c56f3c926b801e9deee62e9c2950bc1815f43
maboeckmann <madmatti.de@gmail.com>
2025-07-28 23:48:48 +0200

    Rewriting this entire plugin to make it easier to add more sound files

--- LES.cs
+++ LES.cs
@@ -1,104 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Reflection;
 using Assets.Api;
 using BepInEx;
-using HarmonyLib;
 using System.Timers;
 using Assets.Features.Hud;
-using System.IO;
-using UnityEngine;
-using UnityEngine.Networking;
-using System.Collections;
 using BepInEx.Configuration;
-using System.Data;
+using UnityEngine;
+using System.IO;
 
+
+
+/*
+Events
+Leaking: Leak message from OnDisplayGameText (images for the mercs that were received)
+Building a unit: UnitAPI.On(Any)UnitHired
+Queueing a worker (HudAPI)
+Sending a mercenary: ??????
+MiniScoreboard: Bottom left score board
+*/
 namespace LES
 {
-    using P = Plugin;
-
     [BepInProcess("Legion TD 2.exe")]
     [BepInPlugin("d8784b16-df1f-4dbb-ae2d-42f1355cb3a2", "LES", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
-        private Timer timer = new Timer();
-        private AudioClip ruby;
-        private AudioClip fucked;
-        private ConfigEntry<bool> configRubyOnLaunch;
+        public readonly Timer timer = new();
+        public GameObject soundMaster;
+    
+        public ConfigEntry<bool> configRubyOnLaunch;
 
-        public void Awake() {
+        public void Awake()
+        {
+            Console.WriteLine("Awake called for LES plugin");
+            soundMaster = new GameObject("SoundMaster");
+            DontDestroyOnLoad(soundMaster);
             configRubyOnLaunch = Config.Bind("Memes", "Ruby on launch", true, "Shout Ruby Ruben after 20 seconds of launching the game");
 
-            try {
+            try
+            {
                 Patch();
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 Logger.LogError($"Error while injecting or patching: {e}");
                 throw;
             }
 
-            this.timer.Interval = 20000;
+            timer.Interval = 20000;
 
-            this.timer.Elapsed += init;
-            this.timer.Enabled = true;
+            timer.Elapsed += init;
+            timer.Enabled = true;
 
-            string basePath = AppDomain.CurrentDomain.BaseDirectory;
-            FileInfo rubyPath = new FileInfo(basePath + $@"\bepinex\ruby.mp3");
-            FileInfo fuckedPath = new FileInfo(basePath + $@"\bepinex\fucked.mp3");
+            SoundEffect effect = soundMaster.AddComponent<SoundEffect>();
 
-            List<FileInfo> files = new List<FileInfo>();
-            files.Add(rubyPath);
-            files.Add(fuckedPath);
+            FileInfo rubyFileInfo = SoundEffect.GetFileInfoFromName("ruby.mp3");
+            FileInfo fuckedFileInfo = SoundEffect.GetFileInfoFromName("fucked.mp3");
 
-            foreach (FileInfo file in files)
-            {
-                if (!file.Exists)
-                {
-                    String fileName = Path.GetFileName(file.Name);
-                    try
-                    {
-                        string[] resources = Assembly.GetExecutingAssembly().GetManifestResourceNames();
-                        string resource = resources.First(r => r.EndsWith(fileName));
-                        if (resource != null)
-                        {
-                            Logger.LogInfo("Found this resource: " + resource);
-                            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource))
-                            {
-                                Logger.LogInfo("Opened readstream to read this badaboio");
-                                using (FileStream fileStream = file.OpenWrite())
-                                {
-                                    stream.CopyTo(fileStream);
-                                }
-                            }
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Logger.LogError(e.ToString());
-                        Logger.LogError(e.StackTrace);
-                    }
-                }
-            }
-
-            StartCoroutine(GetAudioClip(rubyPath));
-            Logger.LogInfo("Attempting to load mp3 file " + rubyPath.ToString());
-
-            StartCoroutine(GetAudioClip(fuckedPath));
-            Logger.LogInfo("Attempting to load mp3 file " + fuckedPath.ToString());
+            soundMaster.GetComponent<SoundEffect>().Initialize("ruby.mp3");
+            StartCoroutine(effect.GetAudioClip(rubyFileInfo));
+            soundMaster.GetComponent<SoundEffect>().Initialize("fucked.mp3");
+            StartCoroutine(effect.GetAudioClip(fuckedFileInfo));
 
             Logger.LogInfo($"Plugin LES is loaded!");
         }
 
-        public void init(object source, System.Timers.ElapsedEventArgs eArgs)
+        public void init(object source, ElapsedEventArgs eArgs)
         {
-            if (this.ruby != null && configRubyOnLaunch.Value)
+            Console.WriteLine("init called for LES plugin");
+
+            if (configRubyOnLaunch.Value)
             {
-                SfxApi.PlayGlobalSound(this.ruby);
+                SoundEffect.Play("ruby.mp3", 60f);
             }
 
-            HudApi.OnDisplayGameText += (string header, string content, float duration, string image) =>
+            /*
+            HudApi.OnQueueHireUnit += (ushort player, short unit, string unitType) =>
+            {
+                Console.WriteLine("Mercenary was queued up but not yet hired. Player: " + player + ", Unit: " + unit + ", Type: " + unitType);
+            };
+            UnitApi.OnAnyUnitHired += (ushort player, short unit, string unitType) =>
+            {
+                Console.WriteLine("Any Unit was hired. Player: " + player + ", Unit: " + unit + ", Type: " + unitType);
+            };
+            UnitApi.OnUnitHired += (ushort player, short unit, string unitType) =>
+            {
+                Console.WriteLine("Unit was hired. Player: " + player + ", Unit: " + unit + ", Type: " + unitType);
+            };
+            HudApi.OnQueueTrainUnit += (ushort player, short unit, string unitType) =>
+            {
+                Console.WriteLine("Worker was queued up Player: " + player + ", Unit: " + unit + ", Type: " + unitType);
+            };
+            */
+
+            HudApi.OnDisplayGameText += (header, content, duration, image) =>
             {
                 if (content.Contains("cleared"))
                 {
@@ -107,14 +100,16 @@ namespace LES
                 if (content.Contains("leak"))
                 {
                     Console.WriteLine("Somebody leaked. Here's the content: " + content);
+                    //Extract the leak percentage from the content
                     var match = System.Text.RegularExpressions.Regex.Match(content, @"\((\d+)%\)");
                     if (match.Success)
                     {
                         string percentage = match.Groups[1].Value; // "50"
                         int percentageInt = int.Parse(percentage);
-                        if(percentageInt >= 100 && this.fucked != null)
+                        if (percentageInt >= 100)
                         {
-                            SfxApi.PlayGlobalSound(this.fucked);
+                            Console.WriteLine("Leak percentage is 100% or more, trying to play sound effect.");
+                            SoundEffect.Play("fucked.mp3", 60f);
                         }
                         Console.WriteLine("Leak percentage: " + percentage);
                     }
@@ -123,13 +118,9 @@ namespace LES
                     {
                         int player = int.Parse(content.Split(')')[0].Split('(')[1]);
                         string name = Scoreboard.GetName(ushort.Parse(player.ToString()));
-                        if (name.Contains(":Ruben B") && this.ruby != null)
+                        if (name.Contains(":Ruben B"))
                         {
-                            SfxApi.PlayGlobalSound(this.ruby);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ruby ruben: Player leaked, but it wasn't ruby ruben :sad " + name);
+                            SoundEffect.Play("ruby.mp3", 60f);
                         }
                     }
                     catch (Exception ex)
@@ -138,45 +129,23 @@ namespace LES
                         Console.WriteLine(ex.Message);
                         Console.WriteLine(ex.StackTrace);
                     }
-                    Console.WriteLine("Leak registered!");
-                    Console.WriteLine(content);
-
-
                 }
             };
 
-            this.timer.Enabled = false;
+            timer.Enabled = false;
         }
 
-        IEnumerator GetAudioClip(FileInfo file)
+        public void OnDestroy()
         {
-            UnityWebRequest webRequest = UnityWebRequestMultimedia.GetAudioClip(
-                file.FullName.ToString(), AudioType.MPEG);
-
-            yield return webRequest.SendWebRequest();
-
-            if (webRequest.result == UnityWebRequest.Result.ConnectionError)
-            {
-                Debug.Log(webRequest.error);
-            }
-            else
-            {
-                AudioClip clip = DownloadHandlerAudioClip.GetContent(webRequest);
-                clip.name = file.Name;
-                if(file.Name.Contains("fucked"))
-                    this.fucked = clip;
-                else if (file.Name.Contains("ruby"))
-                    this.ruby = clip;
-            }
-        }
-        public void OnDestroy() {
             UnPatch();
         }
 
-        private void Patch() {
+        private void Patch()
+        {
         }
 
-        private void UnPatch() {
+        private void UnPatch()
+        {
         }
     }
 }
---
+++ SoundEffect.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Reflection;
+using Assets.Api;
+using System.IO;
+using UnityEngine.Networking;
+using System.Collections;
+using BepInEx.Logging;
+using System.Collections.Generic;
+
+namespace LES
+{
+    public class SoundEffect : MonoBehaviour
+    {
+        ManualLogSource Logger => BepInEx.Logging.Logger.CreateLogSource("LES.SoundEffect");
+        public static readonly string basePath = AppDomain.CurrentDomain.BaseDirectory + "\\bepinex\\";
+
+        public static Dictionary<string, AudioClip> soundEffects = [];
+        public static Dictionary<string, float> soundTimeouts = [];
+
+        public static void Play(string clipName, float timeout)
+        {
+            soundEffects.TryGetValue(clipName, out AudioClip clip);
+            if (clip == null)
+            {
+                Console.WriteLine($"Sound effect {clipName} not found in soundEffects dictionary.");
+                return;
+            }
+
+            Console.WriteLine($"Requested to play sound effect: {clipName}");
+            float lastPlayed = soundTimeouts.TryGetValue(clipName, out float lastTime) ? lastTime : 0f;
+            if (lastPlayed == 0f || Time.time - lastPlayed >= timeout)
+            {
+                SfxApi.PlayGlobalSound(clip);
+                Console.WriteLine($"Playing sound effect: {clipName}, setting last played time to {Time.time}");
+                soundTimeouts[clipName] = Time.time; // Update last played time
+            }
+            else
+            {
+                Console.WriteLine($"Sound effect {clipName} is on cooldown. Last played at {lastPlayed}, current time is {Time.time}");
+            }
+        }
+
+        public static FileInfo GetFileInfoFromName(string fileName)
+        {
+            return new FileInfo(basePath + fileName);
+        }
+
+        public void Initialize(string fileName)
+        {
+            Console.WriteLine("Creating sound effect for " + fileName);
+            FileInfo fileInfo = new(basePath + $@"\bepinex\" + fileName);
+
+            // Check if the audio file exists, if not, try to load it from embedded resources
+            if (!fileInfo.Exists)
+            {
+                Console.WriteLine($"Sound effect file {fileName} does not exist at {fileInfo.FullName}. Attempting to load from embedded resources.");
+                try
+                {
+                    string[] resources = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+                    string resource = resources.First(r => r.EndsWith(fileName));
+                    if (resource != null)
+                    {
+                        Logger.LogInfo("Found this resource: " + resource);
+                        using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource);
+                        Logger.LogInfo("Opened readstream to read this badaboio for " + fileName);
+                        using FileStream fileStream = fileInfo.OpenWrite();
+                        stream.CopyTo(fileStream);
+                        Console.WriteLine($"Sound effect file {fileName} copied to {fileInfo.FullName} from embedded resources.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error loading sound effect from embedded resources: {e.Message}");
+                    Logger.LogError(e.ToString());
+                    Logger.LogError(e.StackTrace);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Sound effect file {fileName} already exists at {fileInfo.FullName}.");
+            }
+        }
+
+        
+        public IEnumerator GetAudioClip(FileInfo fileInfo)
+        {
+            UnityWebRequest webRequest = UnityWebRequestMultimedia.GetAudioClip(
+                fileInfo.FullName.ToString(), AudioType.MPEG);
+
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Console.WriteLine("Error loading sound effect from " + fileInfo.FullName + ": " + webRequest.error);
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                Console.WriteLine("Successfully loaded sound effect from " + fileInfo.FullName);
+                soundEffects[fileInfo.Name] = DownloadHandlerAudioClip.GetContent(webRequest);
+            }
+        }
+    }
+}
